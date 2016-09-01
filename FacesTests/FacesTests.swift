69efb935c3e5702fd6b4a7f15da57ee410e1b057
Richard Kelly II <richard.kellyii@capitalone.com>
2016-09-01 15:11:10 -0400

    Initial Commit

---
+++ FacesTests/FacesTests.swift
@@ -0,0 +1,36 @@
+//
+//  FacesTests.swift
+//  FacesTests
+//
+//  Created by Kelly II, Richard W. on 9/1/16.
+//  Copyright © 2016 Kelly II, Richard W. All rights reserved.
+//
+
+import XCTest
+@testable import Faces
+
+class FacesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
